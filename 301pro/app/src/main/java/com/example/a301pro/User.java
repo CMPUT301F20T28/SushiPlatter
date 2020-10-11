ab0cc90f4dc9281306d66e3e695d963fba647d2b
Jiabin Fan <jiabin@ualberta.ca>
2020-10-11 14:52:41 -0600

    jia bin's change

---
+++ 301pro/app/src/main/java/com/example/a301pro/User.java
@@ -0,0 +1,21 @@
+package com.example.a301pro;
+
+class User {
+    private String userName;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public User(String userName, String email, String password, String firstName, String lastName, String phoneNumber) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+}
--- 301pro/app/src/main/java/com/example/a301pro/register.java
+++ 301pro/app/src/main/java/com/example/a301pro/register.java
@@ -1,10 +1,16 @@
 package com.example.a301pro;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class register extends AppCompatActivity {
+    private User newUser;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,5 +18,23 @@ public class register extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
         //隐藏title
         AppCompatAcitiviy:getSupportActionBar().hide();
+
+
+        final EditText userFirstName = findViewById(R.id.first_name);
+        final EditText userLastName = findViewById(R.id.last_name);
+        final EditText userPhone = findViewById(R.id.phone_num);
+        final EditText userEmail = findViewById(R.id.Email);
+        final EditText userUserName = findViewById(R.id.text_username);
+        final EditText userPassword = findViewById(R.id.text_password);
+
+        String firstName = userFirstName.getText().toString();
+        String lastName = userLastName.getText().toString();
+        String phoneNumber = userPhone.getText().toString();
+        String email = userEmail.getText().toString();
+        String userName = userUserName.getText().toString();
+        String password = userPassword.getText().toString();
+
+        newUser = new User(userName,email,password,firstName,lastName,phoneNumber);
+
     }
 }
