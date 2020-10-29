59e3625dadced05129e6e22ceea35556aa4290c8
zhininghjl <zhininghjl@gmail.com>
2020-10-29 13:33:02 -0600

    add intent test dependencies + test classes for login activity

---
+++ 301pro/app/src/androidTest/java/com/example/a301pro/LoginIntentTest.java
@@ -0,0 +1,50 @@
+package com.example.a301pro;
+
+import android.app.Activity;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginIntentTest {
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<login> rule =
+            new ActivityTestRule<>(login.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+    /**
+     * Gets the Activity
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception{
+        Activity activity = rule.getActivity();
+    }
+
+
+    /**
+     * Closes the activity after each test
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+}
