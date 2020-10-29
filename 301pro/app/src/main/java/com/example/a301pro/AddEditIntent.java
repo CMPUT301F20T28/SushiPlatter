a1831a732e7de58e400803afaa359f98c6e1052f
shanzhizhang <shanzhi1@ualberta.ca>
2020-10-29 00:08:45 -0600

    11

--- 301pro/app/src/main/java/com/example/a301pro/AddEditIntent.java
+++ 301pro/app/src/main/java/com/example/a301pro/AddEditIntent.java
@@ -22,7 +22,6 @@ public class AddEditIntent extends AppCompatActivity {
         setContentView(R.layout.add_edit_intent);
         // Get the Intent that started this activity and extract the string
         //隐藏title
-        //
         AppCompatAcitiviy:getSupportActionBar().hide();
         Button canmera = findViewById(R.id.scan_description);
         Window window = this.getWindow();
