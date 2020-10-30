846a4bfea4f7c952ffecb9b470e0bb72b2ae5ada
“Qianqiu <qianqiu@ualberta.ca>
2020-10-30 16:53:52 -0600

    login with email

--- 301pro/app/src/main/java/com/example/a301pro/login.java
+++ 301pro/app/src/main/java/com/example/a301pro/login.java
@@ -6,14 +6,25 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.githang.statusbar.StatusBarCompat;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
     private User login_user;
+    protected FirebaseAuth mAuth;
+   
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,9 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.login);
         // Get the Intent that started this activity and extract the string
         //隐藏title
+        mAuth = FirebaseAuth.getInstance();
+
+
         AppCompatAcitiviy:getSupportActionBar().hide();
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -30,7 +44,8 @@ public class login extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getBaseContext(),MainActivity.class);
                 startActivity(intent);
-                finish();
+                log_in();
+//                finish();
             }
         });
 
@@ -43,6 +58,40 @@ public class login extends AppCompatActivity {
             }
         });
         }
+    public void log_in() {
+        EditText emailView = findViewById(R.id.text_username);
+        EditText passwordView = findViewById(R.id.text_password);
+        String email = emailView.getText().toString();
+        String password = passwordView.getText().toString();
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(login.this, "Please ensure you have " +
+                    "filled out all the fields.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            validate(email, password);
+        }
+    }
+
+    public void validate(String driverEmail, String driverPassword) {
+        mAuth.signInWithEmailAndPassword(driverEmail, driverPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(login.this,
+                                    "Login Successful.", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            //Log.w(TAG, "signInWithEmail: failure", task.getException());
+                            Toast.makeText(login.this,
+                                    "Login failed. Please check your info and try again.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 
 
 }
