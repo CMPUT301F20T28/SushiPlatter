1cbb6afd5afa2d45d7204e94af7dd3691a3a7895
Jinyu Shen <jshen4@ualberta.ca>
2020-11-04 17:14:56 -0700

    fix RegisterTest

--- 301pro/app/src/androidTest/java/com/example/a301pro/RegisterTest.java
+++ 301pro/app/src/androidTest/java/com/example/a301pro/RegisterTest.java
@@ -30,22 +30,36 @@ public class RegisterTest {
     }
 
 
-    //test if the register button actually work
+    //test if the login button in Register class actually work
     @Test
     public void checkLoginSwitch() {
-        solo.assertCurrentActivity("Wrong activity",login.class);
-        solo.clickOnText("LOGIN");
-        solo.waitForActivity("login",10000);
         solo.assertCurrentActivity("Wrong activity",register.class);
+        solo.clickOnButton("Login");
+        solo.waitForActivity("login",10000);
+        solo.assertCurrentActivity("Wrong activity",login.class);
         solo.sleep(10000);
+
     }
 
-    //test if login button actually work
+    //test if Register button actually work 有bug,每次需要去firebase删除该用户才能重复运行,或者新建一个不重名的
     @Test
     public void checkDoRegisterButton() {
-        solo.assertCurrentActivity("Wrong activity",login.class);
+        solo.assertCurrentActivity("Wrong activity",register.class);
+
+        solo.enterText((EditText)solo.getView(R.id.first_name),"intent_testing1");
+        solo.enterText((EditText)solo.getView(R.id.last_name),"intent_testing1");
+        solo.enterText((EditText)solo.getView(R.id.phone_num),"123456");
+        solo.enterText((EditText)solo.getView(R.id.Email),"intent1@testing.ca");
+        solo.enterText((EditText)solo.getView(R.id.text_username),"intent_testing1");
+        solo.enterText((EditText)solo.getView(R.id.text_password),"123456");
+        solo.enterText((EditText)solo.getView(R.id.text_password_check),"123456");
+
+        solo.sleep(10000);
+
+
         solo.clickOnButton("DONE REGISTER");
-        solo.waitForActivity("MainActivity",10000);
+
+        solo.waitForActivity("login",10000);
         solo.assertCurrentActivity("Wrong activity",login.class);
         solo.sleep(10000);
 
