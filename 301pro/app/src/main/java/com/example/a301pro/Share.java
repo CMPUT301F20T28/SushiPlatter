7bbb5c31c601187a1b3be1e2a4bf0b70ad10f1a4
Jiabin Fan <jiabin@ualberta.ca>
2020-11-05 19:21:38 -0700

    xie ge dan

--- 301pro/app/src/main/java/com/example/a301pro/Share.java
+++ 301pro/app/src/main/java/com/example/a301pro/Share.java
@@ -9,7 +9,15 @@ public class Share implements Serializable {
     private String des;
     private String sit;
     private String owner;
-
+    /**
+     * This constructor initializes the property of a Share
+     * @param imageId image id of the book
+     * @param book_name name of the book
+     * @param des description of the book
+     * @param sit status of the book
+     * @param bookID id of the book
+     * @param owner owner of the book
+     */
     public Share(String bookID,String imageId,String book_name, String des, String sit, String owner) {
         this.bookID = bookID;
         this.imageId =imageId;
@@ -18,51 +26,88 @@ public class Share implements Serializable {
         this.sit = sit;
         this.owner = owner;
     }
-
+    /**
+     * This allows the outside to get the owner's name of a book
+     * @return ID of a book
+     */
     public String getBookID() {
         return bookID;
     }
-
+    /**
+     * This set the image id of a book
+     * @param bookID bookid to be set
+     */
     public void setBookID(String bookID) {
         this.bookID = bookID;
     }
-
+    /**
+     * This allows the outside to get the image id of a book
+     * @return image id of a book
+     */
     public String getImageId() {
         return imageId;
     }
-
+    /**
+     * This set the image id of a book
+     * @param imageId image id to be set
+     */
     public void setImageId(String imageId) {
         this.imageId = imageId;
     }
-
+    /**
+     * This allows the outside to get the image id of a book
+     * @return image id of a book
+     */
     public String getBook_name() {
         return book_name;
     }
+    /**
+     * This allows the outside to get the description of a book
+     * @return description of a book
+     */
 
     public String getDes() {
         return des;
     }
-
+    /**
+     * This allows the outside to get the owner's name of a book
+     * @return owner's name of a book
+     */
     public String getSit() {
         return sit;
     }
-
+    /**
+     * This allows the outside to get the status of a book
+     * @return status of a book
+     */
     public String getOwner() {
         return owner;
     }
-
+    /**
+     * This set the description of a book
+     * @param book_name description to be set
+     */
     public void setBook_name(String book_name) {
         this.book_name = book_name;
     }
-
+    /**
+     * This set the description of a book
+     * @param des description to be set
+     */
     public void setDes(String des) {
         this.des = des;
     }
-
+    /**
+     * This set the status of a book
+     * @param status status to be set
+     */
     public void setSit(String sit) {
         this.sit = sit;
     }
-
+    /**
+     * This set the name of a book
+     * @param book_name name of a book to be set
+     */
     public void setOwner(String owner) {
         this.owner = owner;
     }
