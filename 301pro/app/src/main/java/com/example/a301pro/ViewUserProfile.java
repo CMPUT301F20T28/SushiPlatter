ce7e2137bca4c8f923f8dab632c85f3e9dad6b39
jiale <534218580hu@gmail.com>
2020-11-05 18:24:41 -0700

    update the user information to userProfile

--- 301pro/app/src/main/java/com/example/a301pro/ViewUserProfile.java
+++ 301pro/app/src/main/java/com/example/a301pro/ViewUserProfile.java
@@ -1,20 +1,29 @@
 package com.example.a301pro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ViewUserProfile extends AppCompatActivity {
-    TextView usernameShow, phoneShow, emailShow;
+    TextView userFullNameShow, phoneShow, emailShow, UserNameShow;
     Button edit, profileQuit;
+    String Tag = "ViewUserProfile";
+    String firstName, lastName, phoneNumber, email;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +31,47 @@ public class ViewUserProfile extends AppCompatActivity {
         AppCompatAcitiviy:getSupportActionBar().hide();
         final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        usernameShow = findViewById(R.id.user_name_display);
+        userFullNameShow = findViewById(R.id.user_name_display);
         phoneShow = findViewById(R.id.phone_num_display);
         emailShow = findViewById(R.id.user_email_display);
         edit = findViewById(R.id.edit_profile);
         profileQuit = findViewById(R.id.profile_quit);
 
-        CollectionReference collectionReference = db.collection("User");
-        DocumentReference docRef = collectionReference.document(getUserID());
+        DocumentReference docRef = db.collection("Users").document(getUserID());
+        mAuth = FirebaseAuth.getInstance();
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document != null) {
+
+                        firstName = document.getString("firstName");
+                        Log.i(Tag,"First name"+firstName);
+
+                        lastName = document.getString("lastName");
+                        Log.i(Tag,"Last name"+lastName);
+
+                        email = document.getString("email");
+                        Log.i(Tag,"email"+document.getString("email"));
+
+                        phoneNumber = document.getString("phoneNumber");
+                        Log.i(Tag,"phoneNumber"+document.getString("phoneNumber"));
+
+                        phoneShow.setText(phoneNumber);
+                        emailShow.setText(email);
+                        userFullNameShow.setText(firstName + " " + lastName);
+
+                    } else {
+                        Log.d(Tag, "No such document");
+                    }
+                } else {
+                    Log.d(Tag, "get failed with ", task.getException());
+                }
+            }
+        });
+
 
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,4 +95,5 @@ public class ViewUserProfile extends AppCompatActivity {
     protected String getUserID() {
         return FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
+
 }
