e7a5c2811753632a0aeaa7a9d8959150a6c43f3f
jiabinfan <jiabin@ualberta.ca>
2020-11-27 12:10:57 -0700

    1

--- 301pro/app/src/main/java/com/example/a301pro/AddEditIntent.java
+++ 301pro/app/src/main/java/com/example/a301pro/AddEditIntent.java
@@ -46,7 +46,7 @@ import java.io.ByteArrayOutputStream;
 
 /**
  * This class provides functionality of adding a new book to the collection of the user,
- * and editing the data of a selected own book.。
+ * and editing the data of a selected own book.
  */
 public class AddEditIntent extends AppCompatActivity {
     private EditText bookName;
